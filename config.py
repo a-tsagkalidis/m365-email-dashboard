c5265477243ab3f2f420f4c489dee7b2173ecf25
Argyrios Tsagkalidis <arg.tsag@gmail.com>
2025-06-04 16:39:04 +0300

    Update: config.py

--- config.py
+++ config.py
@@ -1,12 +1,17 @@
 import os
 from dotenv import load_dotenv
 
+
 if os.path.exists('.env.maildash.api'):
-    load_dotenv(dotenv_path='.env.maildash.api')
+    load_dotenv('.env.maildash.api')
 
 class Config:
     CLIENT_ID = os.getenv('CLIENT_ID')
     CLIENT_SECRET = os.getenv('CLIENT_SECRET')
     TENANT_ID = os.getenv('TENANT_ID')
+    MS_ONLINE_URL = os.getenv('MSONLINE_URL', 'https://login.microsoftonline.com')
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    REDIRECT_PATH = '/getAToken'
+    SCOPE = ['Mail.Read', 'Mail.Send']
 
 app_config = Config()
